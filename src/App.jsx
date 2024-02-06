f20337f144a11560d340f9f9d2db4f692074050a
hydrol0x <mrjacobry@gmail.com>
2024-02-06 06:43:54 -0500

    headings

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ const App = () => {
   return (
     <>
       <Router>
-        <NavbarComponent />
+        {/* <NavbarComponent /> */}
         <Routes>
           <Route path="/" element={<Home />} />
           {/* <Route path="/about" element={<About />} /> */}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,8 +5,18 @@ const Home = () => {
   return (
     <>
       <Container>
-        <Row>
-          <h1>The Society for Future Programmers</h1>
+        <Row className="home-superheading">
+          <h3>We are the</h3>
+        </Row>
+        <Row className="home-heading">
+          <h1>Society for Future Programmers</h1>
+        </Row>
+        <Row className="intro-paragraph">
+          <p>
+            <em>
+              Inspiring teens aruond Florida in programming and computer science
+            </em>
+          </p>
         </Row>
       </Container>
     </>
