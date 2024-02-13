f98ae5bb933932c094a76fe9be6c18b0724dc475
hydrol0x <mrjacobry@gmail.com>
2024-02-13 08:50:39 -0500

    casing

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Home from "./pages/home";
+import Home from "./pages/Home";
 import "./App.css";
 import NavbarComponent from "./components/Nav";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
